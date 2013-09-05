21fb556af07bb2a8408c61f57e0960ff8c471748
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-09-05 15:12:35 -0700

    Condensed HTTPModels

--- Cows-RESTful-API-MVC/CowsAPI/Models/HttpModel.php
+++ Cows-RESTful-API-MVC/CowsAPI/Models/HttpModel.php
@@ -2,19 +2,28 @@
 
 namespace CowsAPIModels;
 
-abstract class HttpModel	{
+class HttpModel	{
 	
 	protected $curl;
 	protected $method;
-	
-	abstract function setParams();
 	
 	public function setCookieFile($file)	{
 		setOption(CURLOPT_COOKIEJAR, $file);
 		setOption(CURLOPT_COOKIEFILE, $file);
 	}
 	
-	public function setURL($url)	{
+	public function setMethod($method)	{
+		$this->method = $method;
+		if ($method != "POST")	{
+			$this->curl->set_option(CURLOPT_POSTFIELDS, array());
+		}
+	}
+	
+	public function setPostParameters($params)	{
+		$this->curl->set_option(CURLOPT_POSTFIELDS, $params);
+	}
+	
+	protected function setURL($url)	{
 		$curl->setOption(CURLOPT_URL, $url);
 	}
 	
