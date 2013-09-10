b934f1f9a16521c1ab7d5545e7595f60904e3cba
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-09-10 14:17:55 -0700

    Added URL Builder and the consts to power it

---
+++ Cows-RESTful-API-MVC/CowsAPI/Data/Config.php
@@ -0,0 +1,27 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_NAME", "cowsrest");
+define("DB_TABLE", "cowsrest");
+define("DB_TABLE_LOG", "cowsrestlog");
+define("DB_USER", "root");
+define("DB_PASS", "");
+// DO NOT EDIT BELOW THIS LINE
+define("COWS_LOGIN_PATH","/Account/LogOn");
+define("COWS_EVENT_PATH","/Event/Create");
+define("COWS_BASE_EVENT_PATH","/Event");
+define("COWS_LOGOUT_PATH","/Account/LogOff");
+define("COWS_DELETE_PATH","/event/Delete");
+define("COWS_RSS_PATH","/event/atom");
+define("CAS_PROXY_PATH","https://cas.ucdavis.edu/cas/proxy");
+define("CAS_LOGOUT_PATH","https://cas.ucdavis.edu/cas/logout");
+define("COWS_BASE_PATH","http://cows.ucdavis.edu/");
+
+define("ERROR_GENERIC", "-1");
+define("ERROR_CAS", "-2");
+define("ERROR_EVENT", "-3");
+define("ERROR_CURL", "-4");
+define("ERROR_RSS", "-5");
+define("ERROR_PARAMETERS","-6");
+define("ERROR_DB","-7");
+define("ERROR_COWS","-8");
+?>
---
+++ Cows-RESTful-API-MVC/CowsAPI/Utility/URLBuilder.php
@@ -0,0 +1,49 @@
+<?php
+namespace CowsAPI\Utility;
+
+require_once '../Data/Config.php';
+
+class URLBuilder	{
+	
+	public function __construct()	{
+		
+	}
+	
+	public function getCasProxyURL($serviceName, $tgc)	{
+		
+		return CAS_PROXY_PATH . "?pgt=" . urlencode($tgc) . "&service=" . urlencode($serviceName);
+	}
+	
+	public function getCasLogoutUrl()	{
+		return CAS_PROXY_PATH;
+	}
+	
+	public function getCowsEventUrl($siteId)	{
+		return COWS_BASE_PATH . $siteId . COWS_BASE_EVENT_PATH;
+	}
+	
+	public function getCowsLoginUrl($siteId, $ticket)	{
+		return COWS_BASE_PATH . $siteId . COWS_LOGIN_PATH . "?returnUrl=http://cows.ucdavis.edu/". $siteId .  "&ticket=" . urlencode($ticket);
+	}
+	
+	public function getCowsLogoutUrl($siteId)	{
+		return COWS_BASE_PATH . $siteId . COWS_LOGOUT_PATH;
+	}
+	
+	public function getCowsRssUrl($siteId, $params)	{
+		return COWS_BASE_PATH . $siteId . COWS_RSS_PATH . "?" . http_build_query($params);
+	}
+	
+	public function getCowsEventIdUrl($siteId, $eventId)	{
+		return COWS_BASE_PATH . $siteId . COWS_BASE_EVENT_PATH . "/details/" . $eventId;
+	}
+	
+	public function getEventDeleteUrl($siteId)	{
+		return COWS_BASE_PATH . $siteId . COWS_DELETE_PATH;
+	}
+	
+	public function getCowsBaseUrl($siteId)	{
+		return COWS_BASE_PATH . $siteId;
+	}
+}
+?>
