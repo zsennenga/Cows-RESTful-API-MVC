5eb9bc8ad74d4d6cd0ffee626d7c3aa8d671f8c9
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-09-10 15:10:15 -0700

    Completed DataMapperFactory and DomainObjectFactory

--- Cows-RESTful-API-MVC/CowsAPI/Models/DataMapperFactory.php
+++ Cows-RESTful-API-MVC/CowsAPI/Models/DataMapperFactory.php
@@ -3,6 +3,30 @@ namespace CowsAPI\Models;
 
 class DataMapperFactory	{
 	
+	private $db;
+	private $curl;
+	
+	public function __construct(DbWrapper $db, CurlWrapper $curl)	{
+		$this->db = $db;
+		$this->curl = $curl;
+	}
+	
+	public function get($className)	{
+		$className = "\\CowsApi\\Models\\" . $className;
+		return new $className($this->curl, $this->db);
+	}
+	
+	public function basicPost($url, $params)	{
+		$this->curl->setOption(CURLOPT_URL, $url);
+		$this->curl->setOption(CURLOPT_POSTFIELDS, $params);
+		$this->curl->setOption(CURLOPT_CUSTOMREQUEST, "POST");
+		
+		$out = $this->curl->execute(); 
+		
+		$this->curl->setOption(CURLOPT_POSTFIELDS, null);
+		
+		return $out;
+	}
 }
 
 ?>
--- Cows-RESTful-API-MVC/CowsAPI/Models/DomainObjectFactory.php
+++ Cows-RESTful-API-MVC/CowsAPI/Models/DomainObjectFactory.php
@@ -2,6 +2,15 @@
 namespace CowsAPI\Models;
 
 class DomainObjectFactory	{
+	
+	public function __construct()	{
+		
+	}
+	
+	public function get($className)	{
+		$className = "\\CowsAPI\\Models\\" . $className;
+		return new $className();
+	}
 
 }
 ?>
