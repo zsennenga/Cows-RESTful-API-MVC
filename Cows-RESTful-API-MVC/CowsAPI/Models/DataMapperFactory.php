a4529b264f6539d0b6b1f83c477c4e2f573eb5d2
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-09-11 13:08:18 -0700

    Moved basicPost to it's own class

--- Cows-RESTful-API-MVC/CowsAPI/Models/DataMapperFactory.php
+++ Cows-RESTful-API-MVC/CowsAPI/Models/DataMapperFactory.php
@@ -5,28 +5,24 @@ class DataMapperFactory	{
 	
 	private $db;
 	private $curl;
+	private $publicKey;
 	
-	public function __construct(DbWrapper $db, CurlWrapper $curl)	{
+	public function __construct(DbWrapper $db, CurlWrapper $curl, $publicKey)	{
 		$this->db = $db;
 		$this->curl = $curl;
+		$this->publicKey = $publicKey;
 	}
 	
 	public function get($className)	{
 		$className = "\\CowsApi\\Models\\" . $className;
-		return new $className($this->curl, $this->db);
-	}
+		
+		$sm = null;
+		if ($className != "SessionManager") 
+			$sm = new SessionManager($this->curl, $this->db, $this->publicKey, null);
 	
-	public function basicPost($url, $params)	{
-		$this->curl->setOption(CURLOPT_URL, $url);
-		$this->curl->setOption(CURLOPT_POSTFIELDS, $params);
-		$this->curl->setOption(CURLOPT_CUSTOMREQUEST, "POST");
-		
-		$out = $this->curl->execute(); 
-		
-		$this->curl->setOption(CURLOPT_POSTFIELDS, null);
-		
-		return $out;
+		return new $className($this->curl, $this->db, $this->publicKey, $sm);
 	}
+
 }
 
 ?>
